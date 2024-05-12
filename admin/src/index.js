657972bddcee6bb7d4eedbddabf9cd1988a3ccee
CodeWaveWithAsante <132752907+CodeWaveWithAsante@users.noreply.github.com>
2024-05-12 18:34:31 +0530

    UPDATED 12052024

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,17 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "@mantine/tiptap/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <MantineProvider>
+          <App />
+        </MantineProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
